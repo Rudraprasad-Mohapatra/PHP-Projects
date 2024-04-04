7be91d5b93da0a73690f6b727e9d98dab2dc80ee
Rudraprasad-Mohapatra <rudraprasadmohapatra51@gmail.com>
2024-04-04 23:56:30 +0530

    Form for PHP code completed

---
+++ Project-1-in-PHP-Travel-Form/index.php
@@ -0,0 +1,32 @@
+<?php
+if ($isset($_POST['name'])) {
+    $server = "localhost";
+    $username = "root";
+    $password = "";
+
+    $con = mysqli_connect($server, $username, $password);
+
+    if (!$con) {
+        die("Connection to this database failed due to" . mysqli_connect_errno());
+    }
+    // echo "Success connecting to the db";
+
+    $name = $_POST['name'];
+    $age = $_POST['age'];
+    $gender = $_POST['gender'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $other = $_POST['desc'];
+
+    $sql = "INSERT INTO `trip` (`name`, `age`, `gender`, `email`, `phone`, `other`, `dt`) VALUES ('$name', '$age', '$gender', '$email', '$phone', '$other', current_timestamp());";
+
+    echo $sql;
+
+    if ($con->query($sql) == true) {
+        echo "Successfully inserted";
+    } else {
+        echo "ERROR: $sql <br> $con -> error";
+    }
+
+    $con->close();
+}
